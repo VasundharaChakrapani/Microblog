6b88768a010ce851c81b5d0993f7a6057bebf137
Vasundhara Chakrapani <vasundharac07@gmail.com>
2025-12-23 00:04:12 +0530

    new

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'https://microblog-k5e9.onrender.com/',
+  baseURL: 'https://microblog-k5e9.onrender.com',
   headers: { 'Content-Type': 'application/json' },
   withCredentials: true
 });
